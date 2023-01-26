2f19630b4bc7d978b2c6e49f59fb2e9cb0ae6f07
jzandona <jason.zandona@runpanther.io>
2023-01-26 14:45:18 -0800

    cleanup gsuite detections from branch

--- panther_detections/providers/gsuite/__init__.py
+++
@@ -1,9 +0,0 @@
-from typing import Literal
-
-from . import queries, rules, sample_logs
-from ._shared import *
-
-
-def use_all_with_defaults() -> None:
-    rules.calendar_made_public()
-    rules.passthrough_rule()
--- panther_detections/providers/gsuite/rules/__init__.py
+++
@@ -1,2 +0,0 @@
-from .calendar_made_public import *
-from .passthrough_rule import *

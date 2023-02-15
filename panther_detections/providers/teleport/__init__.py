6f8a18af029a36db7035757eb7c60a5b27b1c877
jzandona <jason.zandona@runpanther.io>
2023-02-15 05:27:24 -0800

    updated for consistency

--- panther_detections/providers/teleport/__init__.py
+++ panther_detections/providers/teleport/__init__.py
@@ -1,12 +1,15 @@
-from typing import Literal
+from typing import Literal, List, Union
+from panther_sdk import detection
 
 from . import rules, sample_logs
 from ._shared import *
 
 
-def use_all_with_defaults() -> None:
-    rules.auth_errors()
-    rules.create_user_accounts()
-    rules.network_scanning()
-    rules.scheduled_jobs()
-    rules.suspicious_commands()
+def use_all_with_defaults() -> List[Union[detection.Rule]]:
+    return [
+        rules.auth_errors(),
+        rules.create_user_accounts(),
+        rules.network_scanning(),
+        rules.scheduled_jobs(),
+        rules.suspicious_commands(),
+    ]

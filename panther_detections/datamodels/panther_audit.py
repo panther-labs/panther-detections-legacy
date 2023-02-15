54190080e9cc74ee6d7e9f2e98eb7993de4d96ea
Carrie Pascale <carrie.pascale@panther.io>
2023-02-15 17:03:02 -0500

    data model 2

--- panther_detections/datamodels/panther_audit.py
+++ panther_detections/datamodels/panther_audit.py
@@ -1,37 +1,46 @@
-# import panther_event_type_helpers as event_type
+from panther_detections.utils import event_type_helpers as event_type
+from panther_sdk import PantherEvent, schema
 
-# audit_log_type_map = {
-#     "CREATE_USER": event_type.USER_ACCOUNT_CREATED,
-#     "DELETE_USER": event_type.USER_ACCOUNT_DELETED,
-#     "UPDATE_USER": event_type.USER_ACCOUNT_MODIFIED,
-#     "CREATE_USER_ROLE": event_type.USER_GROUP_CREATED,
-#     "DELETE_USER_ROLE": event_type.USER_GROUP_DELETED,
-#     "UPDATE_USER_ROLE": event_type.USER_ROLE_MODIFIED,
-# }
+audit_log_type_map = {
+    "CREATE_USER": event_type.USER_ACCOUNT_CREATED,
+    "DELETE_USER": event_type.USER_ACCOUNT_DELETED,
+    "UPDATE_USER": event_type.USER_ACCOUNT_MODIFIED,
+    "CREATE_USER_ROLE": event_type.USER_GROUP_CREATED,
+    "DELETE_USER_ROLE": event_type.USER_GROUP_DELETED,
+    "UPDATE_USER_ROLE": event_type.USER_ROLE_MODIFIED,
+}
 
 
-# def get_event_type(event):
-#     audit_log_type = event.get("actionName")
-#     matched = audit_log_type_map.get(audit_log_type)
-#     if matched is not None:
-#         return matched
-#     return None
+def get_event_type(event):
+    audit_log_type = event.get("actionName")
+    matched = audit_log_type_map.get(audit_log_type)
+    if matched is not None:
+        return matched
+    return None
 
-# AnalysisType: datamodel
-# LogTypes: 
-#   - Panther.Audit
-# DataModelID: Standard.Panther.Audit
-# Filename: panther_audit_data_model.py
-# DisplayName: Panther Audit Logs
-# Enabled: true
-# Mappings:
-#   - Name: source_ip
-#     Path: sourceIP
-#   - Name: user_agent
-#     Path: userAgent
-#   - Name: actor_user
-#     Path: $.actor.attributes.email
-#   - Name: user
-#     Path: $.actionParams.input.email
-#   - Name: event_type
-#     Method: get_event_type
+
+def panther_audit() -> schema.DataModel:
+    return schema.DataModel(
+        data_model_id="standard.panther.audit",
+        name="Panther Audit Model",
+        enabled=True,
+        log_type=schema.LogTypePantherAudit,
+        mappings=[
+            schema.DataModelMapping(
+                name="source_ip",
+                path="sourceIP",
+            ),
+            schema.DataModelMapping(
+                name="user_agent",
+                path="userAgent",
+            ),
+            schema.DataModelMapping(
+                name="actor_user",
+                path="actionParams.input.email",
+            ),
+            schema.DataModelMapping(
+                name="event_type",
+                func=get_event_type,
+            ),
+        ],
+    )
